7e841b0037f0297ffc411eed42e5a2cd7c75f28f
KeeYoo <poronko2045@gmail.com>
2022-06-11 17:42:45 +0300

    Start

---
+++ DelegatesLinq/DelegatesLinq/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace DelegatesLinq
+{
+    internal class Program
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
